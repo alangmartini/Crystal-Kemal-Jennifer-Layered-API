1d668bed18abd76da01efdcc9c8f108738821e8c
gmaralan <gmartinialan@gmail.com>
2023-06-18 12:39:03 -0400

    chore: added scripts that setup envs for test db

--- app/backend/sam.cr
+++ app/backend/sam.cr
@@ -3,6 +3,18 @@ require "sam"
 require "./migrations/*"
 load_dependencies "jennifer"
 
-# ...
+desc "Run the tests"
+task "spec" do
+  ENV["APP_ENV"] = "test"
+  ENV["KEMAL_ENV"]= "test"
+  system "crystal spec"
+end
+
+desc "Drop test database and re setup"
+task "setup_test" do
+  ENV["APP_ENV"] = "test"
+  system "crystal sam.cr db:drop"
+  system "crystal sam.cr db:setup"
+end
 
 Sam.help
