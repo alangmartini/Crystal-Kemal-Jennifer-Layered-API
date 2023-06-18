143ef6b06dc70a50fbfc1c71fea7cdaac3f64528
gmaralan <gmartinialan@gmail.com>
2023-06-18 10:31:39 -0400

    chore: changed db:create and db:migrate to simpler db:setup

--- app/backend/src/app.cr
+++ app/backend/src/app.cr
@@ -4,10 +4,9 @@ require "./routes/travels.route.cr"
 
 include UserRoutes
 
-# Create and populate the database. This is done here
+# Create, migrate and populate the database. This is done here
 # instead of inside Dockerfile because it needs
-# the db to be created and running.
-Process.run("crystal", ["sam.cr", "db:create"])
-Process.run("crystal", ["sam.cr", "db:migrate"])
+# the mysql service to be created and running.
+Process.run("crystal", ["sam.cr", "db:setup"])
 
 Kemal.run
