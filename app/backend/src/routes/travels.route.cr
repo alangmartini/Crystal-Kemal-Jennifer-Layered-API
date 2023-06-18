fe3f8cfa49f8dcf8ca14e642657f576366475346
gmaralan <gmartinialan@gmail.com>
2023-06-18 11:55:44 -0400

    feat: TDD implemented raw POST /travel-plans

--- app/backend/src/routes/travels.route.cr
+++ app/backend/src/routes/travels.route.cr
@@ -1,31 +1,42 @@
 # /routes/user_routes.cr
 require "kemal"
 require "../models/travel_plan.model"
+require "../models/rel_travel_plans_travel_stops.model"
 require "jennifer"
 
 module UserRoutes
-  get "/" do |env|
+  post "/" do |env|
 
-    # travel_plan = TravelPlan.create({
-    #   name: "My Travel Plan",
-    #   travel_stops: [
-    #     TravelStop.create({
-    #       name: "Stop 1",
-    #       location: "Location 1"
-    #     }),
-    #     TravelStop.create({
-    #       name: "Stop 2",
-    #       location: "Location 2"
-    #     })
-    #   ]
-    # })
-
-    travel_plan = TravelPlans.create({ travel_stops: 1 })
+    body = env.request.body.try &.gets_to_end
+    if body
+      parsed_body = JSON.parse(body)
+      travel_stops = parsed_body["travel_stops"].as_a
+    else
+      travel_stops = { "travel_stops": [] of Int64 }
+    end
     
-    puts travel_plan
-    {
-      "id": 1,
-      "travel_stops": [1, 2]
-    }.to_json
+    
+    travel_plan = TravelPlans.create({} of String => Int64)
+
+    travel_id = travel_plan.id
+
+    if travel_id
+      travel_stops.each do |travel_stop|
+        puts "travel is #{travel_stop}"
+        puts typeof(travel_stop)
+
+        value = travel_stop.is_a?(Symbol) ? travel_stop.to_s.to_i64? : travel_stop.as_i64?
+
+        RelTravelPlansTravelStops.create({
+          travel_plan_id: travel_id,
+          travel_stop_id: value
+        })    
+      end
+
+      {
+      "id": travel_id,
+      "travel_stops": travel_stops
+      }.to_json
+    end 
   end
 end
