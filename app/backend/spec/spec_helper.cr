e04c768e249113f8dfb6d02e3c4a21506c91fd20
gmaralan <gmartinialan@gmail.com>
2023-06-18 13:36:23 -0400

    chore: setup for test db done succesfully

--- app/backend/spec/spec_helper.cr
+++ app/backend/spec/spec_helper.cr
@@ -4,6 +4,8 @@ require "../config/config.cr"
 require "../src/app"
 require "./mocks/travel.mock.cr"
 
+Process.run("crystal", ["sam.cr", "setup_test"])
+
 Spec.before_each do
   Jennifer::Adapter.default_adapter.begin_transaction
 end
