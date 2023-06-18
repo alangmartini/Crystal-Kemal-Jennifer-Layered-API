08224e633ed46edb6fa6cbd66a6f4be31907baf2
gmaralan <gmartinialan@gmail.com>
2023-06-18 15:41:12 -0400

    fix: fixed double jsonfication

--- app/backend/spec/app_spec.cr
+++ app/backend/spec/app_spec.cr
@@ -2,15 +2,15 @@ require "./spec_helper"
 
 def create_travel_plan()
   header = HTTP::Headers{"Content-Type" => "application/json"}
-  travel_stops_1 = TravelData::CREATE_NEW_TRAVEL_MOCK_1
+  travel_stops_1 = TravelData::CREATE_NEW_TRAVEL_MOCK_1.to_json
 
   post "/travel-plans", headers: header, body: travel_stops_1
 end
 
 def create_travel_plans()
   header = HTTP::Headers{"Content-Type" => "application/json"}
-  travel_stops_1 = TravelData::CREATE_NEW_TRAVEL_MOCK_1
-  travel_stops_2 = TravelData::CREATE_NEW_TRAVEL_MOCK_2
+  travel_stops_1 = TravelData::CREATE_NEW_TRAVEL_MOCK_1.to_json
+  travel_stops_2 = TravelData::CREATE_NEW_TRAVEL_MOCK_2.to_json
 
   post "/travel-plans", headers: header, body: travel_stops_1
   post "/travel-plans", headers: header, body: travel_stops_2
@@ -20,7 +20,7 @@ describe "Travels::Kemal:App" do
   it "correctly creates a new Travel Plan" do
     create_travel_plan()
 
-    response.body.should eq TravelData::CREATED_TRAVEL_MOCK_1
+    response.body.should eq TravelData::POST_CREATED_TRAVEL_MOCK_1.to_json
     response.status_code.should eq 201
   end
 
@@ -31,7 +31,11 @@ describe "Travels::Kemal:App" do
 
     get "/travel-plans", headers: header
 
-    response.body.should eq [TravelData::CREATED_TRAVEL_MOCK_1, TravelData::CREATED_TRAVEL_MOCK_2].to_json
+    response.body.should eq [
+                              TravelData::GET_CREATED_TRAVEL_MOCK_1,
+                              TravelData::GET_CREATED_TRAVEL_MOCK_2
+                            ].to_json
+                            
     response.status_code.should eq 200
   end
 
--- app/backend/spec/mocks/travel.mock.cr
+++ app/backend/spec/mocks/travel.mock.cr
@@ -3,19 +3,17 @@
 module TravelData
   CREATE_NEW_TRAVEL_MOCK_1 = {
     "travel_stops": [1, 2]
-  }.to_json
+  }
   
   CREATE_NEW_TRAVEL_MOCK_2 = {
     "travel_stops": [10, 20]
-  }.to_json
+  }
   
-  CREATED_TRAVEL_MOCK_1 = {
-    "id": 1,
-    "travel_stops": [1, 2]
-  }.to_json
+  POST_CREATED_TRAVEL_MOCK_1 = { "id": 1, "travel_stops": [1, 2] }
 
-  CREATED_TRAVEL_MOCK_2 = {
-    "id": 2,
-    "travel_stops": [10, 20]
-  }.to_json
+  POST_CREATED_TRAVEL_MOCK_2 = { "id": 2, "travel_stops": [10, 20] }
+
+  GET_CREATED_TRAVEL_MOCK_1 = { "id": 2, "travel_stops": [1, 2] }
+
+  GET_CREATED_TRAVEL_MOCK_2 = { "id": 3, "travel_stops": [10, 20] }
 end
