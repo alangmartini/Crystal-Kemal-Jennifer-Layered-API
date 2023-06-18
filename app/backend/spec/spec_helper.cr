95cb8e4b1f2b2b8ce39b0d30f8cda2153a60feef
gmaralan <gmartinialan@gmail.com>
2023-06-18 12:39:43 -0400

    chore: added rollback fn of jennifer for tests as github says.

--- app/backend/spec/spec_helper.cr
+++ app/backend/spec/spec_helper.cr
@@ -1,4 +1,15 @@
 require "spec-kemal"
 require "spec"
+require "../config/config.cr"
 require "../src/app"
 require "./mocks/travel.mock.cr"
+
+Spec.before_each do
+  Jennifer::Adapter.default_adapter.begin_transaction
+end
+
+Spec.after_each do
+  Jennifer::Adapter.default_adapter.rollback_transaction
+end
+
+
