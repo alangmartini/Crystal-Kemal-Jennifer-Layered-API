3184ec70fded6b7e07d1ddfea7de8763ae0a6001
gmaralan <gmartinialan@gmail.com>
2023-06-18 11:54:54 -0400

    fix: missed .model. suffix

--- app/backend/src/models/rel_travel_plans_travel_stops.cr
+++
@@ -1,9 +0,0 @@
-class RelTravelPlansTravelStops < Jennifer::Model::Base
-  table_name = :rel_travel_plans_travel_stops
-
-  mapping(
-    id: { type: Int64, primary: true, auto_increment: true},
-    travel_plans_id: { type: Int64 },
-    travel_stops: { type: Int64 }
-  )
-end
