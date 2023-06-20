6b4176007f388a2fd3a03cee38b347e6e3bb3644
gmaralan <gmartinialan@gmail.com>
2023-06-20 06:37:17 -0400

    doc: documented class GetAllTravelPlansConstructor

--- app/backend/src/entities/TravelPlans/GetAllTravelPlansConstructor.entity.cr
+++ app/backend/src/entities/TravelPlans/GetAllTravelPlansConstructor.entity.cr
@@ -1,4 +1,7 @@
+
+# Get `TravelPlans` from DB and construct them to `ConstructedTravelPlan` objects
 class GetTravelPlansResponseConstructor
+  # Get all `TravelPlans` from DB and construct them to `RawTravelPlan` objects
   def self.get_all_raw_travel_plans_from_db() : Array(RawTravelPlan)
     # In Jennifer, .all only refers to a query,
     # needing thus .to_json to execute it.
@@ -11,20 +14,21 @@ class GetTravelPlansResponseConstructor
     raw_travel_plans
   end
 
+  # Get all `TravelStops` related to specific `TravelPlans` from DB
   def self.get_travel_stops_from_db(raw_travel_plan : RawTravelPlan)
     travel_stops_id =  RelTravelPlansTravelStops
         .all
         .where { _travel_plan_id == raw_travel_plan.id }
         .to_json(only: %w[travel_stop_id])
 
-      travel_stops_ids = JSON
+      travel_stops_ids: Array(Int64) = JSON
         .parse(travel_stops_id)
         .as_a.map { |row| row["travel_stop_id"].as_i.to_i64 }
 
-      puts typeof(travel_stops_ids[0])
       travel_stops_ids
   end
 
+  # Join `RawTravelPlan` as its TravelStops and construct them to `ConstructedTravelPlan` objects
   def self.get_all_constructed_travel_plans() : Array(ConstructedTravelPlan)
     raw_travel_plans = self.get_all_raw_travel_plans_from_db()
 
