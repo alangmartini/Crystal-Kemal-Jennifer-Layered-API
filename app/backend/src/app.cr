2bc1b90d2ef68a12cfc925e130dbb4f6e07fde9d
gmaralan <gmartinialan@gmail.com>
2023-06-21 12:56:32 -0400

    feat: TDD implemented DELETE /travel_plans/:id

--- app/backend/src/app.cr
+++ app/backend/src/app.cr
@@ -4,6 +4,9 @@ require "./routes/travels.route.cr"
 
 include TravelPlansRoute
 
+get "/" do |env|
+  env.response.status_code = 200
+end
 # Create, migrate and populate the database. This is done here
 # instead of inside Dockerfile because it needs
 # the mysql service to be created and running.
