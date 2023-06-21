ac886c2f2530b9e0e4e4d10be849de0adbeb0b88
gmaralan <gmartinialan@gmail.com>
2023-06-21 07:03:20 -0400

    fix: added excepton catcher for Error treatment

--- app/backend/src/routes/travels.route.cr
+++ app/backend/src/routes/travels.route.cr
@@ -22,7 +22,7 @@ module UserRoutes
       travel_stops = { "travel_stops": [] of Int32 }
     end
     
-    
+    # TravelPlans only consists of a auto_increment ID
     travel_plan = TravelPlans.create({} of String => Int32)
 
     travel_id = travel_plan.id
@@ -48,61 +48,70 @@ module UserRoutes
   end
 
   get "/travel_plans" do |env|
-    # Cast String as Bool
-    optimise = env.params.query["optimize"] == "true"
-    expand = env.params.query["expand"] == "true"
+    begin
+      # Cast String as Bool
+      optimise = env.params.query["optimize"]? == "true"
+      expand = env.params.query["expand"]? == "true"
 
-    # Get TravelPlans from DB and construct them to ConstructedTravelPlan objects
-    constructed_travel_plans : Array(ConstructedTravelPlan) =
-      GetTravelPlansResponseConstructor
-        .get_all_constructed_travel_plans()
-    puts "is false #{constructed_travel_plans.empty?}"
+      # Get TravelPlans from DB and construct them to ConstructedTravelPlan objects
+      constructed_travel_plans : Array(ConstructedTravelPlan) =
+        GetTravelPlansResponseConstructor
+          .get_all_constructed_travel_plans()
 
-    if constructed_travel_plans.empty? || (!optimise && !expand)
+      if constructed_travel_plans.empty? || (!optimise && !expand)
+        env.response.content_type = "application/json"
+        env.response.status_code = 200
+    
+        next constructed_travel_plans.to_json
+      end
+
+      travel_stops =
+        GetTravelPlansResponseConstructor
+          .get_all_unique_travel_stops_ids(
+            constructed_travel_plans
+        )
+
+      graph_ql_query = RickAndMortyApiClient.new(travel_stops)
+      response : JSON::Any = graph_ql_query.execute()
+      
+      locations : Array(Location) = response["data"]["locationsByIds"]
+        .as_a.map { |location| Location.from_json(location.to_json) }
+
+      simplified_locations : Array(SimplifiedLocation) = LocationSimplificator
+        .simplify(locations)
+
+      if optimise
+        optimised_locations :
+          Array(SimplifiedLocation) =
+            LocationOptimiser
+              .optimise(simplified_locations)
+
+        simplified_locations = optimised_locations
+      end
+
+      expand_and_or_optimised_constructed_travel_plans :
+        Array(
+          ConstructedTravelPlan |
+          ConstructedExpandedTravelPlan
+        ) =
+            TravelPlanOptimiser.construct_travel_plans(
+              expand,
+              constructed_travel_plans,
+              simplified_locations
+            )
+
       env.response.content_type = "application/json"
       env.response.status_code = 200
-  
-      next constructed_travel_plans.to_json
+
+      next expand_and_or_optimised_constructed_travel_plans.to_json
+    rescue e
+      env.response.content_type = "application/json"
+      env.response.status_code = 500
+
+      next {
+        "message" => "Something went wrong #{e}",
+        "status_code" => 500
+      }.to_json
     end
-
-    travel_stops =
-      GetTravelPlansResponseConstructor
-        .get_all_unique_travel_stops_ids(
-          constructed_travel_plans
-      )
-
-    graph_ql_query = RickAndMortyApiClient.new(travel_stops)
-    response : JSON::Any = graph_ql_query.execute()
-    
-    locations : Array(Location) = response["data"]["locationsByIds"]
-      .as_a.map { |location| Location.from_json(location.to_json) }
-
-    simplified_locations : Array(SimplifiedLocation) = LocationSimplificator
-      .simplify(locations)
-
-    if optimise
-      optimised_locations :
-        Array(SimplifiedLocation) =
-          LocationOptimiser
-            .optimise(simplified_locations)
-
-      simplified_locations = optimised_locations
-    end
-
-    expand_and_or_optimised_constructed_travel_plans :
-      Array(
-        ConstructedTravelPlan |
-        ConstructedExpandedTravelPlan
-      ) =
-          TravelPlanOptimiser.construct_travel_plans(
-            expand,
-            constructed_travel_plans,
-            simplified_locations
-          )
-
-    env.response.content_type = "application/json"
-    env.response.status_code = 200
-
-    next expand_and_or_optimised_constructed_travel_plans.to_json
   end
 end
