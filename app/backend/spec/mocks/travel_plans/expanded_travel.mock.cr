8f6c0870554679ca83e64d264b80277325e4aa51
gmaralan <gmartinialan@gmail.com>
2023-06-21 07:07:20 -0400

    chore: moved  folders and created mock for new entities

---
+++ app/backend/spec/mocks/travel_plans/expanded_travel.mock.cr
@@ -0,0 +1,85 @@
+module ExpandedLocationsMocks
+  DIMENSION_C_137_LOCATIONS = [
+    {
+      "id": 1,
+      "dimension": "Dimension C-137",
+      "name": "Earth (C-137)",
+      "type": "Planet"
+    },
+    {
+      "id": 5,
+      "dimension": "Dimension C-137",
+      "name": "Anatomy Park",
+      "type": "Microverse"
+    },
+    {
+      "id": 18,
+      "dimension": "Dimension C-137",
+      "name": "Mr. Goldenfold's dream",
+      "type": "Dream"
+    }
+  ]
+
+  DIMENSION_UNKOWN_LOCATIONS = [
+    {
+      "id": 2,
+      "dimension": "unknown",
+      "name": "Abadango",
+      "type": "Cluster"
+    },
+    {
+      "id": 3,
+      "dimension": "unknown",
+      "name": "Citadel of Ricks",
+      "type": "Space station"
+    },
+    {
+      "id": 7,
+      "dimension": "unknown",
+      "name": "Immortality Field Resort",
+      "type": "Resort"
+    },
+    {
+      "id": 11,
+      "dimension": "unknown",
+      "name": "Bepis 9",
+      "type": "Planet"
+    }
+  ]
+
+  DIMENSIONS_REPLACEMENT_LOCATIONS = [
+    {
+      "id": 9,
+      "dimension": "Replacement Dimension",
+      "name": "Purge Planet",
+      "type": "Planet"
+    },
+    {
+      "id": 19,
+      "dimension": "Replacement Dimension",
+      "name": "Gromflom Prime",
+      "type": "Planet"
+    },
+    {
+      "id": 20,
+      "dimension": "Replacement Dimension",
+      "name": "Earth (Replacement Dimension)",
+      "type": "Planet"
+    }
+  ]
+
+
+  DIMENSION_1 = DIMENSION_C_137_LOCATIONS[0]
+  DIMENSION_5 = DIMENSION_C_137_LOCATIONS[1]
+  DIMENSION_18 = DIMENSION_C_137_LOCATIONS[2]
+
+  DIMENSION_2 = DIMENSION_UNKOWN_LOCATIONS[0]
+  DIMENSION_3 = DIMENSION_UNKOWN_LOCATIONS[1]
+  DIMENSION_7 = DIMENSION_UNKOWN_LOCATIONS[2]
+  DIMENSION_11 = DIMENSION_UNKOWN_LOCATIONS[3]
+
+  DIMENSION_9 = DIMENSIONS_REPLACEMENT_LOCATIONS[0]
+  DIMENSION_19 = DIMENSIONS_REPLACEMENT_LOCATIONS[1]
+  DIMENSION_20 = DIMENSIONS_REPLACEMENT_LOCATIONS[2]
+
+end
--- app/backend/spec/mocks/travel_plans/travel.mock.cr
+++ app/backend/spec/mocks/travel_plans/travel.mock.cr
@@ -6,14 +6,15 @@ module TravelData
   }
   
   CREATE_NEW_TRAVEL_MOCK_2 = {
-    "travel_stops": [10, 20]
+    "travel_stops": [10, 3]
   }
   
   POST_CREATED_TRAVEL_MOCK_1 = { "id": 1, "travel_stops": [1, 2] }
 
-  POST_CREATED_TRAVEL_MOCK_2 = { "id": 2, "travel_stops": [10, 20] }
+  POST_CREATED_TRAVEL_MOCK_2 = { "id": 2, "travel_stops": [10, 3] }
 
   GET_CREATED_TRAVEL_MOCK_1 = { "id": 2, "travel_stops": [1, 2] }
 
-  GET_CREATED_TRAVEL_MOCK_2 = { "id": 3, "travel_stops": [10, 20] }
+  GET_CREATED_TRAVEL_MOCK_2 = { "id": 3, "travel_stops": [10, 3] }
+
 end
