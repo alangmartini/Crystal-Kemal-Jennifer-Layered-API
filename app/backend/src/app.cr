afc56ed97362074bb24b29ed72191d55fd232d91
gmaralan <gmartinialan@gmail.com>
2023-06-21 08:27:49 -0400

    fix: wrong module name

--- app/backend/src/app.cr
+++ app/backend/src/app.cr
@@ -2,7 +2,7 @@ require "kemal"
 require "../config/config.cr"
 require "./routes/travels.route.cr"
 
-include UserRoutes
+include TravelPlansRoute
 
 # Create, migrate and populate the database. This is done here
 # instead of inside Dockerfile because it needs
