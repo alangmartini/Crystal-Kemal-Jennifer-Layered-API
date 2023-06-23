2d528d8e6631b5c68c70426f49444fff0ae79169
gmaralan <gmartinialan@gmail.com>
2023-06-23 08:37:25 -0400

    refactor: changed create logic for more performatic import

--- app/backend/src/services/TravelPlansService.service.cr
+++ app/backend/src/services/TravelPlansService.service.cr
@@ -24,19 +24,11 @@ class TravelPlansService < AbstractTravelPlanService
       .from_json(created_travel_plans.to_json)
 
     # With the id in hands, we can now create the travel stops
-    travel_stops = [] of Int32
-    travel_stops_json
-      .travel_stops
-      .each do |travel_stop|
-        created_travel_stop = @TravelStopsModel
-          .create({
-            travel_plan_id: raw_travel_plan.id,
-            travel_stop_id: travel_stop
-          })
+    travel_stops = create_travel_stops(
+      travel_stops_json,
+      raw_travel_plan.id
+    )
 
-        travel_stops << created_travel_stop.travel_stop_id
-      end
-    
     constructed_travel_plan = ConstructedTravelPlan.new(
       id: raw_travel_plan.id,
       travel_stops: travel_stops
@@ -45,10 +37,31 @@ class TravelPlansService < AbstractTravelPlanService
     constructed_travel_plan
   end
 
-  def self.create_travel_stops(
-    travel_stops : Array(Int32),
+  # For each travel_stop supplied, create a new RelTravelPlansTravelStops
+  # in DB, relationing it to the TravelPlan
+  def create_travel_stops(
+    travel_stops_json : TravelStopsJSON,
     id : Int32
-    )
+    ) : Array(Int32)
+    rel_travel_plans = [] of RelTravelPlansTravelStops
 
+    travel_stops_json
+      .travel_stops
+      .each do |travel_stop|
+        created_travel_stop = @TravelStopsModel
+          .new({
+            travel_plan_id: id,
+            travel_stop_id: travel_stop
+          })
+
+        rel_travel_plans << created_travel_stop
+      end
+
+    @TravelStopsModel.import(rel_travel_plans)
+
+    travel_stops = rel_travel_plans.map { |rel_travel_plan|
+      rel_travel_plan.travel_stop_id }
+
+    travel_stops  
   end
 end
