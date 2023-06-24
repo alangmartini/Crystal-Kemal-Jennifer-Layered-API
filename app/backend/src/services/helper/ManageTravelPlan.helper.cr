ebe87ad818ac48389032211e6c9eed9ee751494d
gmaralan <gmartinialan@gmail.com>
2023-06-24 07:16:39 -0400

    refactor: created separated module for managing travel plans

---
+++ app/backend/src/services/helper/ManageTravelPlan.helper.cr
@@ -0,0 +1,68 @@
+module TravelPlansService
+  # Module for operations involving the reconstruction
+  # of `ConstructedTravelPlan` into its optimised
+  # and/or expanded version.
+  module ManageTravelPlan
+    alias SimplifiedLocation = RickAndMorty::Entities::SimplifiedLocation
+    # Reconstruction of `ConstructedTravelPlan` into its
+    # optimised version: `ConstructedOptimisedTravelPlan`.
+    def reconstruct_optimised_travel_plans(
+      constructed_travel_plans : Array(ConstructedTravelPlan),
+      simplified_locations : Array(SimplifiedLocation)
+    ) : Array(ConstructedOptimisedTravelPlan)
+      constructed_travel_plans.map do |constructed_travel_plan|
+        travel_stops : Array(Int32) = ManageTravelStops
+          .get_unexpanded_travel_stops(
+            simplified_locations,
+            constructed_travel_plan.travel_stops
+          )
+
+        ConstructedOptimisedTravelPlan.new(
+          constructed_travel_plan.id,
+          travel_stops,
+        )
+      end
+    end
+    # Reconstruction of `ConstructedTravelPlan` into its
+    # expanded version: `ConstructedExpandedTravelPlan`.
+    def reconstruct_expanded_travel_plans(
+      constructed_travel_plans : Array(ConstructedTravelPlan),
+      simplified_locations : Array(SimplifiedLocation)
+    ) : Array(ConstructedExpandedTravelPlan)
+      constructed_travel_plans.map do |constructed_travel_plan|
+        expanded_travel_stops : Array(ExpandedTravelStop) =
+            ManageTravelStops
+              .get_expanded_travel_stops(
+                simplified_locations,
+                constructed_travel_plan.travel_stops
+              )
+
+        ConstructedExpandedTravelPlan.new(
+          constructed_travel_plan.id,
+          expanded_travel_stops,
+        )
+      end
+    end
+
+    # Reconstruction of `ConstructedTravelPlan` into its
+    # expanded and optimised version:
+    # `ConstructedExpandedOptimisedTravelPlan`.
+    def reconstruct_expanded_and_optimised_travel_plans(
+      constructed_travel_plans : Array(ConstructedTravelPlan),
+      simplified_locations : Array(SimplifiedLocation)
+      ) : Array(ConstructedExpandedOptimisedTravelPlan)
+      constructed_travel_plans.map do |constructed_travel_plan|
+        expanded_travel_stops : Array(Int32) = ManageTravelStops
+          .get_expanded_travel_stops(
+            simplified_locations,
+            constructed_travel_plan.travel_stops
+          )
+
+          ConstructedExpandedOptimisedTravelPlan.new(
+          constructed_travel_plan.id,
+          travel_stops,
+        )
+      end
+    end
+  end
+end
--- app/backend/src/services/helper/ManageTravelStops.helper.cr
+++ app/backend/src/services/helper/ManageTravelStops.helper.cr
@@ -5,19 +5,32 @@ module TravelPlansService
   # creation or transformations.
   module ManageTravelStops
     alias SimplifiedLocation = RickAndMorty::Entities::SimplifiedLocation
+    
+    # Finds the correspondent locations from the travel stops
+    # and returns them.
+    def self.find_correspondent_locations(
+      simplified_locations : Array(SimplifiedLocation),
+      travel_stops : Array(Int32)
+    )
+      simplified_locations
+        .select { |location| travel_stops
+          .includes?(location.id) }
+    end
 
     # Uses the information from `Location`
     # to expand a TravelStop into a `ExpandedTravelStop`.
     #
     # A expanded travel stop its just a travel_stop (Int32)
     # with details about the location.
-    def self.expand_travel_stops(
-      optimised_simplified_locations : Array(SimplifiedLocation),
+    def self.get_expanded_travel_stops(
+      simplified_locations : Array(SimplifiedLocation),
       travel_stops : Array(Int32)
     ) : Array(ExpandedTravelStop)
-      new_travel_stops  = optimised_simplified_locations
-      .select { |location| travel_stops
-        .includes?(location.id) }
+    
+      new_travel_stops  = find_correspondent_locations(
+        simplified_locations,
+        travel_stops
+      )
       
       new_travel_stops = new_travel_stops.map do |travel_stops|
         ExpandedTravelStop.new(
@@ -31,12 +44,24 @@ module TravelPlansService
       new_travel_stops
     end
 
+    # Gets only the ids from the Locations, which
+    # composes a simple travel stop.
+    def self.get_unexpanded_travel_stops(
+      simplified_locations : Array(SimplifiedLocation),
+      travel_stops : Array(Int32)
+    )
+      self.find_correspondent_locations(
+        simplified_locations,
+        travel_stops
+      ).map { |travel_stop| travel_stop.id }
+    end
+
     # Returns the ExpandedTravelStop to normal TravelStops, which
     # only contain the id.
     def self.unexpand_travel_stops(
       expanded_travel_stops : Array(ExpandedTravelStop)
     ) : Array(Int32)
       expanded_travel_stops.map { |travel_stop| travel_stop.id }
-    end
+    end   
   end
 end
